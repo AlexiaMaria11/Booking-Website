e3adedc54a58328c5f175ec5e6c93ba15c77f2a0
AlexiaMaria11 <oprtitamariaalexia@gmail.com>
2025-08-15 16:01:19 +0300

    Homepage Lists

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,31 @@
+import "./featured.css";
+
+const Featured = () => {
+  return (
+    <div className="featured">
+      <div className="featuredItem">
+        <img src="/images/dublin.jpg" alt="" className="featuredImg" />
+        <div className="featuredTitles">
+          <h1>Dublin</h1>
+          <h2>123 properties</h2>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img src="/images/austin.jpg" alt="" className="featuredImg" />
+        <div className="featuredTitles">
+          <h1>Austin</h1>
+          <h2>501 properties</h2>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img src="/images/reno.jpg" alt="" className="featuredImg" />
+        <div className="featuredTitles">
+          <h1>Reno</h1>
+          <h2>533 properties</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
