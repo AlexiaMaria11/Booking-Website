f4de7a67034cb943e113963f08580abf25dd363d
AlexiaMaria11 <oprtitamariaalexia@gmail.com>
2025-08-16 12:07:43 +0300

    Search Result List

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">AleBook</span>
+        <span className="logo">AleBooking</span>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
