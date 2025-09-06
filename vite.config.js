679a83befff22b401ca7c041545635c6f5ed0911
AlexiaMaria11 <oprtitamariaalexia@gmail.com>
2025-09-06 19:46:30 +0300

    node MongoDB Authentification

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // permite accesul și din rețea
+    port: 8800, // schimbă portul
+  },
+});
