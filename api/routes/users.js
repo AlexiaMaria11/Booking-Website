6b2e3d42789233d42042e1571d1af7d83ac24042
AlexiaMaria11 <oprtitamariaalexia@gmail.com>
2025-09-09 21:08:26 +0300

    Role-based Authorization Verification

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,38 @@
 import express from "express";
+import User from "../models/User.js";
+import { createError } from "../utils/error.js";
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controllers/user.js";
+import { verifyToken, verifyUser, verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+// router.get("/checkauthentication", verifyToken, (req, res, next) => {
+//   res.send("You are authenticated");
+// });
+
+// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//   res.send("You are authenticated and you can delete your account");
+// });
+
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//   res.send("You are authenticated and you can delete all accounts");
+// });
+
+// UPDATE
+router.put("/:id", verifyUser, updateUser);
+
+// DELETE
+router.delete("/:id", verifyUser, deleteUser);
+
+// GET
+router.get("/:id", verifyUser, getUser);
+
+// GET ALL
+router.get("/", verifyAdmin, getUsers);
 
 export default router;
