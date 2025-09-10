04b38f81957ba09f2aac3818ee319df7939b697f
AlexiaMaria11 <oprtitamariaalexia@gmail.com>
2025-09-10 14:32:49 +0300

    Count of Documents

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -51,3 +51,15 @@ export const getHotels = async (req, res, next) => {
     next(err);
   }
 };
+
+export const countByCity = async (req, res, next) => {
+  const cities = req.query.cities.split(",");
+  try {
+    const list = await Promise.all(
+      cities.map((city) => Hotel.countDocuments({ city }))
+    );
+    res.status(200).json(list);
+  } catch (err) {
+    next(err);
+  }
+};
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -2,6 +2,7 @@ import express from "express";
 import Hotel from "../models/Hotel.js";
 import { createError } from "../utils/error.js";
 import {
+  countByCity,
   createHotel,
   deleteHotel,
   getHotel,
@@ -15,16 +16,18 @@ const router = express.Router();
 // CREATE
 router.post("/", verifyAdmin, createHotel);
 
+// GET ALL
+router.get("/", getHotels);
+
+router.get("/countByCity", countByCity);
+
 // UPDATE
 router.put("/:id", verifyAdmin, updateHotel);
 
 // DELETE
-router.delete("/:id", verifyAdmin, deleteHotel);
+router.delete("/find/:id", verifyAdmin, deleteHotel);
 
 // GET
 router.get("/:id", getHotel);
-
-// GET ALL
-router.get("/", getHotels);
 
 export default router;
